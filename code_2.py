d3b454756b7ebc9950a764a44e76cd9cab7c6baa
vistalann0 <146479343+vistalann0@users.noreply.github.com>
2024-02-17 10:38:41 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,16 @@
+    return data;
+        console.log("Random Number: " + item);
+    }
+}
+    for (let i = 0; i < 10; i++) {
+}
+    const data = generateRandomData();
+function main() {
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    });
+    const data = [];
+
+    data.forEach(item => {
+main();
